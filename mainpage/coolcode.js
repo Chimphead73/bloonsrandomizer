a8ac5f2980d90793f1b9ae6f4121b166fa84485a
unknown <chimphead73@gmail.com>
2023-06-06 15:09:34 -0400

    all towers

--- mainpage/coolcode.js
+++ mainpage/coolcode.js
@@ -31,3 +31,42 @@ function getRandomImageHero() {
     // Display the image in the HTML element with id "result"
     document.getElementById("resultHero").src = randomImageUrl;
   }
+
+  function getRandomImageMonkey() {
+    // Array of image URLs
+    var imageUrls = [
+        "assets/000-DartMonkey.png",
+        "assets/000-BoomerangMonkey.png",
+        "assets/000-IceMonkey.png",//3
+        "assets/000-CannonMonkey.png",//4
+        "assets/000-GlueMonkey.png",//5
+        "assets/000-TackMonkey.png",//6
+        "assets/000-SniperMonkey.png",//7
+        "assets/000-SubMonkey.png",//8
+        "assets/000-PirateMonkey.png",//9
+        "assets/000-PlaneMonkey.png",//10
+        "assets/000-HeliMonkey.png",//11
+        "assets/000-MortarMonkey.png",//12
+        "assets/000-DartlingGunMonkey.png",//13
+        "assets/000-WizardMonkey.png",//14
+        "assets/000-SuperMonkey.png",//15
+        "assets/000-NinjaMonkey.png",//16
+        "assets/000-AlchemistMonkey.png",//17
+        "assets/000-DruidMonkey.png",//18
+        "assets/000-FarmMonkey.png",//19
+        "assets/000-SpikeMonkey.png",//20
+        "assets/000-VillageMonkey.png",//21
+        "assets/000-EngineerMonkey.png",//22
+        "assets/000-BeastMonkey.png"//23
+    
+    ];
+
+    // Generate a random index corresponding to the imageUrls array
+    var randomIndex = Math.floor(Math.random() * imageUrls.length);
+
+    // Get the random image URL based on the random index
+    var randomImageUrl = imageUrls[randomIndex];
+
+    // Display the image in the HTML element with id "result"
+    document.getElementById("resultMonkey").src = randomImageUrl;
+  }
